93450b63a04f4ae00280ec38e28184f873d10f32
Mikhailov Eduard <ctapeek@gmail.com>
2026-08-19 22:39:08 +0300

    add database schema and seed data

---
+++ schema.sql
@@ -0,0 +1,37 @@
+CREATE TABLE categories (
+    category_id SERIAL PRIMARY KEY,
+    category_name VARCHAR(50) UNIQUE
+);
+
+CREATE TABLE customers (
+    customer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    phone VARCHAR(20),
+    registration_date TIMESTAMP  DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE products (
+    product_id SERIAL PRIMARY KEY,
+    product_name VARCHAR(80) NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+    category_id INTEGER REFERENCES categories(category_id),
+    stock INTEGER DEFAULT 0
+);
+
+CREATE TABLE orders (
+    order_id SERIAL PRIMARY KEY,
+    customer_id INTEGER REFERENCES customers(customer_id),
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(20) NOT NULL,
+    total_amount NUMERIC(10,2) NOT NULL
+);
+
+CREATE TABLE order_items (
+    order_item_id SERIAL PRIMARY KEY,
+    order_id INTEGER REFERENCES orders(order_id),
+    product_id INTEGER REFERENCES products(product_id),
+    quantity INTEGER NOT NULL,
+    unit_price NUMERIC(10,2) NOT NULL
+);
