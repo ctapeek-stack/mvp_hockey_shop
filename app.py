c0cdfbef2e4e1cb36bca338bb2eedb237e8d6a21
Mikhailov Eduard <ctapeek@gmail.com>
2026-08-20 05:46:55 +0300

    add db connection script

---
+++ app.py
@@ -0,0 +1,30 @@
+import os
+import psycopg
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+conn = psycopg.connect(    
+    host=os.environ["DB_HOST"],
+    port=os.environ["DB_PORT"],
+    dbname=os.environ["DB_NAME"],
+    user=os.environ["DB_USER"],
+    password=os.environ["DB_PASSWORD"],
+)
+
+cur = conn.cursor()
+cur.execute("""
+    SELECT products.product_name, products.price, categories.category_name
+    FROM products
+    JOIN categories ON products.category_id = categories.category_id
+    ORDER BY products.price;
+""")
+
+
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
+cur.close()
+conn.close()
